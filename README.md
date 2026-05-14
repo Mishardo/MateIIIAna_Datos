1341d3e9f1e1f86517f611bb05d74383bf9fd1d5
fsdsfd <tomasmarcos2019@gmail.com>
2026-05-14 14:48:19 -0300

    punto 1 y 2 con comentarios y prints de predicciones añadidos

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # MateIIIAna_Datos
-TP mate
+# 1
+El dataframe que usamos fue [dataframe de indice de limpieza](https://data.buenosaires.gob.ar/dataset/icla-bap/resource/d8cd8ccd-1a4f-49d4-96d0-b138e896b90d)
+Elegimos el mismo ya que no había que escalar demasiado al venir los datos mucho mas explicitamente, sin columnas que generen ruido.
+# 2
+El dataframe muestra el índice de calidad de limpieza adaptado a barrios vulnerables de Argentina. Para medir objetivamente el nivel de limpieza de las vías de circulación y el mobiliario de higiene urbana de los barrios vulnerables de la ciudad.
+
+Cuenta con 4 columnas: medicion,periodo,estratificacion_geografica e indice_de_calidad_de_limpieza_adaptado.
+
+<strong>Medición</strong>: Número de orden de mediciones anuales.
+<strong>Periodo</strong>: Período de tiempo en el que se realizaron las mediciones.
+<strong>estratificacion_geografica</strong>: Niveles de estratificación del ICLa.
+<strong>indice_de_calidad_de_limpieza_adaptado</strong>: Valor del índice obtenido según estratificación.
--- tp.py
+++ tp.py
@@ -2,21 +2,19 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
 
 # Carga de datos
+
 df = pd.read_csv("medicion.csv")
 
 # Codificación de variables categóricas (AHORA INCLUYE periodo)
 df_encoded = pd.get_dummies(df, columns=["estratificacion_geografica", "periodo"], drop_first=True)
-
 # Variables independientes y dependiente
 X = df_encoded.drop(["indice_de_calidad_de_limpieza_adaptado"], axis=1)
 y = df_encoded["indice_de_calidad_de_limpieza_adaptado"]
-
 # Separación correcta
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
@@ -25,11 +23,11 @@ X_train, X_test, y_train, y_test = train_test_split(
 # Modelo
 modelo = LinearRegression()
 modelo.fit(X_train, y_train)
-
 # Predicción
 y_pred = modelo.predict(X_test)
 
 # Evaluación
+print(f"los resultados a predicir son: {y_test}, y la predicción es : {y_pred}" )
 mse = mean_squared_error(y_test, y_pred)
 rmse = np.sqrt(mse)
 
