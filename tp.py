2f71e9de2c5311eef64a28c6c24888a77254cad3
fsdsfd <tomasmarcos2019@gmail.com>
2026-05-14 13:43:23 -0300

    optimizando tp

--- tp.py
+++ tp.py
@@ -1,53 +1,44 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
-import numpy as np
 import seaborn as sns
-import warnings
-import numpy as np
-from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error
-from sklearn.linear_model import Ridge
 
-warnings.simplefilter("ignore")
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+
 
 df = pd.read_csv('medicion.csv', encoding='cp1252', sep=',')
-print(df)
-df["anio"] = df["periodo"].str.extract(r'(\d{4})').astype(int)
-df_train = df[df["anio"].isin([2022, 2023])]
-df_encoded = pd.get_dummies(df_train, columns=["estratificacion_geografica"])
-X = df_encoded.drop(columns=[
-    "indice_de_calidad_de_limpieza_adaptado",
-    "periodo"
-])
+df.info()
+df.describe()
+df.isnull().sum()
+sns.histplot(df["indice_de_calidad_de_limpieza_adaptado"], kde=True)
+plt.show()
+sns.lineplot(x="periodo", y="indice_de_calidad_de_limpieza_adaptado", data=df)
+plt.show()
+df_encoded = pd.get_dummies(df, columns=["estratificacion_geografica"], drop_first=True)
+train = df_encoded[df_encoded["periodo"].isin([2022, 2023])]
+test = df_encoded[df_encoded["periodo"] == 2024]
+X_train = train.drop(["indice_de_calidad_de_limpieza_adaptado"], axis=1)
+y_train = train["indice_de_calidad_de_limpieza_adaptado"]
 
-y = df_encoded["indice_de_calidad_de_limpieza_adaptado"]
-df_2024 = df[df["anio"] == 2023].copy()
+X_test = test.drop(["indice_de_calidad_de_limpieza_adaptado"], axis=1)
+y_test = test["indice_de_calidad_de_limpieza_adaptado"]
+modelo = LinearRegression()
+modelo.fit(X_train, y_train)
+y_pred = modelo.predict(X_test)
+mse = mean_squared_error(y_test, y_pred)
+print("MSE:", mse)
+rmse = np.sqrt(mse)
+print("RMSE:", rmse)
 
-df_2024["anio"] = 2024  # simulamos el futuro
+plt.scatter(y_test, y_pred)
+plt.xlabel("Valor real")
+plt.ylabel("Predicción")
+plt.title("Real vs Predicho")
+plt.show()
+predicciones_2024 = pd.DataFrame({
+    "Real": y_test,
+    "Predicho": y_pred
+})
 
-df_2024 = pd.get_dummies(df_2024, columns=["estratificacion_geografica"])
-
-# Asegurar mismas columnas
-df_2024 = df_2024.reindex(columns=X.columns, fill_value=0)
-model = LinearRegression()
-
-model.fit(X, y)
-predicciones = model.predict(df_2024)
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-
-model.fit(X_train, y_train)
-y_pred = model.predict(X_test)
-
-print(mean_squared_error(y_test, y_pred))
-df_2024 = df[df["anio"] == 2023].copy()
-
-df_2024["anio"] = 2024  # simulamos el futuro
-
-df_2024 = pd.get_dummies(df_2024, columns=["estratificacion_geografica"])
-
-# Asegurar mismas columnas
-df_2024 = df_2024.reindex(columns=X.columns, fill_value=0)
-
-predicciones = model.predict(df_2024)
+print(predicciones_2024.head())
